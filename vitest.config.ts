24cf03d08ae2177550a68c6b8d75e7822ee52da5
BioWellness <biowellness.ar@gmail.com>
2026-06-20 01:05:32 -0300

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vitest/config';
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  envPrefix: ['MEDPLUM_', 'GOOGLE_', 'RECAPTCHA_'],
+  plugins: [react()],
+  preview: {
+    port: 5173,
+    allowedHosts: true,
+    host: 'recepcion.medplum.com.ar',
+  },
+  server: {
+    port: 5173,
+    host: 'localhost',
+  },
   test: {
-    include: ['tests/**/*.test.ts'],
-    environment: 'node',
-    globals: false,
+    environment: 'jsdom',
+    setupFiles: ['./src/test.setup.ts'],
+    globals: true,
+    testTimeout: 120000,
   },
 });
